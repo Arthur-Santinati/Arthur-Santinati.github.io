05f3c35cf836248bf6f49826c657cf43a9ee69fa
arthursantinati <arthursantinati02@outlook.com>
2022-03-03 18:48:31 -0300

    css

---
+++ teste-portfolio/script.js
@@ -0,0 +1,20 @@
+let favoriteTechnology = "Machine learning";
+favoriteTechnology = "Facial recognition";
+console.log(favoriteTechnology); // Facial 
+
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+)
+
+// Sem o template literals:
+console.log('Primeira linha;' + 'Segunda linha;\n' + 'Terceira linha;\n')
+
+
+const myAge = 19;
+console.log(`Welcome ${myAge}!`);
+
+console.log(`opa filho linha um
+chama nela papai
+ufa bebe`)
